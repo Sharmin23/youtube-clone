dbe0fa37de1cd5351157ca12d901564671a2870f
Sharmin Sultana <sharmin9518@gmail.com>
2024-08-11 19:19:47 +0600

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+
+
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      </Routes>
+      <Route path = '/video/:categoryId/:videoId' element={<Video/>}/>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Navbar.css'
+import menu from '../../assets/menu.png'
+import logo from '../../assets/logo.png'
+import search from '../../assets/search.png'
+import upload from '../../assets/upload.png'
+import more from '../../assets/more.png'
+import notification from '../../assets/notification.png'
+import jack from '../../assets/jack.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav  className='flex-div'>
+      <div className='nav-left flex-div'>
+        <img className='menu-icon' src={menu} alt="" />
+        <img className='logo' src={logo} alt="" />
+      </div>
+      <div className="nav-middle flex-div">
+        <div className="search-box flex-div">
+        <input type="text" placeholder = "Search" />
+        <img src= {search} alt="" />
+        </div>
+     </div>
+     <div className="nav-right flex-div">
+        <img src={upload} alt="" />
+        <img src={more} alt="" />
+        <img src={notification}alt="" />
+        <img src={jack} alt="" />
+     </div>
+
+    </nav>
   )
 }
 
